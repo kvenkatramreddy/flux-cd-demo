cd2bb2e31d156bdbf1e0e714e50a00de401f076b
venkat <venkkoll@in.ibm.com>
2022-07-19 18:03:05 +0530

    Initial commit

---
+++ src/main/java/com/example/fluxcddemo/FluxCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fluxcddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxCdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxCdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fluxcddemo/FluxCdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fluxcddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
